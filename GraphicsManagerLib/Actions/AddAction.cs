cd442336f205716f91fe5d4b77d180e88ba6fe8d
Alex <mrclean.durrant@gmail.com>
2015-11-22 21:00:00 -0700

    Added the ability to add and remove animations through Actions

---
+++ GraphicsManagerLib/Actions/AddAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GameGraphicsLib;
+
+namespace GraphicsManagerLib.Actions
+{
+    [Serializable]
+    public class AddAction : IGraphicAction
+    {
+        public string Name { get; set; }
+        public string Drawable { get; set; }
+        public DrawParam DrawParam { get; set; }
+        public GraphicActionType GraphicActionType { get { return GraphicActionType.Add;} }
+    }
+}
---
+++ GraphicsManagerLib/Actions/RemoveAction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GraphicsManagerLib.Actions
+{
+    [Serializable]
+    public class RemoveAction
+    {
+        public string Name { get; set; }
+        public string Drawable { get; set; }
+        public GraphicActionType GraphicActionType { get { return GraphicActionType.Remove; } } 
+    }
+}
